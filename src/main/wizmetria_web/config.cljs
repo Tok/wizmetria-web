83b4338841a4ab882be1b6ee80bb082f96031efd
Zir Teq <zirteq111@gmail.com>
2025-03-23 04:43:58 +0100

    Fix text analysis by disabling worker-based processing

--- src/main/wizmetria_web/config.cljs
+++ src/main/wizmetria_web/config.cljs
@@ -6,7 +6,10 @@
    :min-word-length 3                          ;; Minimum length for a word to be considered
    :processing-delay-ms 30                     ;; Delay between processing chunks for UI responsiveness
    :symmetry-calculation-delay-ms 50           ;; Delay before starting symmetry calculations
-   :default-word "WIZARD"})                    ;; Default word to analyze on startup
+   :default-word "WIZARD"                      ;; Default word to analyze on startup
+   :worker-batch-size 5000                     ;; Number of words to process in each worker batch
+   :worker-enabled false                        ;; Whether to use web workers for symmetry calculations
+   :parallel-batches 4})                       ;; Maximum number of parallel worker batches
 
 ;; UI configuration
 (def ui-config
