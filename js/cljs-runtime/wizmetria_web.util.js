f4ab1b93b2a3c6988bdfc3b00a74e53753d57397
Zir Teq <zirteq111@gmail.com>
2025-03-23 08:05:03 +0100

    Remove js/cljs-runtime from git tracking as it should be ignored

--- js/cljs-runtime/wizmetria_web.util.js
+++
@@ -1,6 +0,0 @@
-goog.provide('wizmetria_web.util');
-wizmetria_web.util.clean = (function wizmetria_web$util$clean(s){
-return clojure.string.replace(clojure.string.upper_case(s),/[^A-Z]/,"");
-});
-
-//# sourceMappingURL=wizmetria_web.util.js.map
