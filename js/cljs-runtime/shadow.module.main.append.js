4741a598ed62661b0cb0bcb2cbbb66def0e96ebe
Zir Teq <zirteq111@gmail.com>
2025-03-23 06:57:50 +0100

    Deploy: 2025-03-23 06:57:50

--- js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { wizmetria_web.core.init(); } catch (e) { console.error("An error occurred when calling (wizmetria-web.core/init)"); console.error(e); }
